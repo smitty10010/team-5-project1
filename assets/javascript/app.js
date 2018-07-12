1b35cd78c2abe28ff39153ccda16825b9c46f4e2
Robert <35673753+smitty10010@users.noreply.github.com>
2018-07-11 21:09:06 -0600

    added initial js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+var map;
+var infowindow;
+
+function initMap() {
+    var denver = {
+        lat: 39.7231759,
+        lng: -105.1296253
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: denver,
+        zoom: 11.49
+    });
+
+    var request = {
+        location: denver,
+        query: 'mountain climbing gyms'
+    };
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
