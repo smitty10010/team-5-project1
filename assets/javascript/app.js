94ffdca03f3f687c061d42779043c515f577665b
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2018-07-14 14:41:43 -0600

    added firebase to js files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCeiT1Q0QtBaOFezcF3IzQSFTiRHnWpheM",
+    authDomain: "team-five-projec-1531180321292.firebaseapp.com",
+    databaseURL: "https://team-five-projec-1531180321292.firebaseio.com",
+    projectId: "team-five-projec-1531180321292",
+    storageBucket: "team-five-projec-1531180321292.appspot.com",
+    messagingSenderId: "91504514776"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
 var map;
 var infowindow;
 var denver;
