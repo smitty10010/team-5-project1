bfc8c2fcabe5f942751f8a7d597d255af0f2cc31
Robert <35673753+smitty10010@users.noreply.github.com>
2018-07-14 15:43:40 -0600

    conflict merge

--- assets/javascript/profile.js
+++ assets/javascript/profile.js
@@ -1,16 +1,3 @@
-function initMap() {
-    denver = {
-        lat: 39.7231759,
-        lng: -105.1296253
-    };
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: denver,
-        zoom: 10
-    });
-
-}
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyCeiT1Q0QtBaOFezcF3IzQSFTiRHnWpheM",
@@ -22,16 +9,3 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
-
-database.ref().once("value", function(snapshot) {
-    if (snapshot.val().q1 === "indoor") {
-
-
-    }
-    if (snapshot.val().q1 === "outdoor") {
-
-    }
-})
-
-$('.tabs').tabs();
-$('.parallax').parallax();
