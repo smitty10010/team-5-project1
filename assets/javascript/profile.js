6f059c8a3f46f270eaaac1df3f48e0f0485b5ad2
Robert <35673753+smitty10010@users.noreply.github.com>
2018-07-14 15:36:07 -0600

    added map

--- assets/javascript/profile.js
+++ assets/javascript/profile.js
@@ -1,3 +1,16 @@
+function initMap() {
+    denver = {
+        lat: 39.7231759,
+        lng: -105.1296253
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: denver,
+        zoom: 10
+    });
+
+}
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyCeiT1Q0QtBaOFezcF3IzQSFTiRHnWpheM",
@@ -6,6 +19,19 @@ var config = {
     projectId: "team-five-projec-1531180321292",
     storageBucket: "team-five-projec-1531180321292.appspot.com",
     messagingSenderId: "91504514776"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+database.ref().once("value", function(snapshot) {
+    if (snapshot.val().q1 === "indoor") {
+
+
+    }
+    if (snapshot.val().q1 === "outdoor") {
+
+    }
+})
+
+$('.tabs').tabs();
+$('.parallax').parallax();
